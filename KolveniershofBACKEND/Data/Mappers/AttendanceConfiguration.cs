6d0eeca70020b58e0735553b9f319ded4d1d49ae
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-14 17:28:40 +0200

    Implement attendance and dayactivity config classes

--- KolveniershofBACKEND/Data/Mappers/AttendanceConfiguration.cs
+++ KolveniershofBACKEND/Data/Mappers/AttendanceConfiguration.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using KolveniershofBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KolveniershofBACKEND.Data.Mappers
 {
-    public class AttendanceConfiguration
+    public class AttendanceConfiguration : IEntityTypeConfiguration<Attendance>
     {
+        public void Configure(EntityTypeBuilder<Attendance> builder)
+        {
+            builder.ToTable("Attendance");
+            builder.HasKey(a => new { a.DayId, a.ActivityId, a.UserId });
+            builder.Property(a => a.DayActivity).IsRequired();
+            builder.Property(a => a.Comment).IsRequired();
+            builder.HasOne(a => a.DayActivity)
+                .WithMany(da => da.Attendances)
+                .IsRequired()
+                // Not sure if this works?
+                .HasForeignKey(a => new { a.DayId, a.ActivityId })
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- KolveniershofBACKEND/Data/Mappers/DayActivityConfiguration.cs
+++ KolveniershofBACKEND/Data/Mappers/DayActivityConfiguration.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using KolveniershofBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KolveniershofBACKEND.Data.Mappers
 {
-    public class DayActivityConfiguration
+    public class DayActivityConfiguration : IEntityTypeConfiguration<DayActivity>
     {
+        public void Configure(EntityTypeBuilder<DayActivity> builder)
+        {
+            builder.ToTable("DayActivity");
+            builder.HasKey(da => new { da.DayId, da.ActivityId });
+            builder.Property(da => da.TimeOfDay).IsRequired();
+            builder.HasOne(da => da.Day)
+                .WithMany(d => d.DayActivities)
+                .IsRequired()
+                .HasForeignKey(da => da.DayId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(da => da.Activity)
+                .WithMany()
+                .IsRequired()
+                .HasForeignKey(da => da.ActivityId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
