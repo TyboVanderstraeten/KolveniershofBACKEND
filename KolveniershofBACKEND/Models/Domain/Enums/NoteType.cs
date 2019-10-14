8606c3d43b606b29b6404f1b2e2f3193947ae381
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-14 14:23:32 +0200

    Add domain class files

---
+++ KolveniershofBACKEND/Models/Domain/Enums/NoteType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Models.Domain
+{
+    public class NoteType
+    {
+    }
+}
