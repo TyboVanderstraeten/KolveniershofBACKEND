f7c9babe9ac66e3a2587ebf9ee0e4935bfd201be
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-16 19:12:53 +0200

    Add concrete repository files

---
+++ KolveniershofBACKEND/Data/Repositories/Concrete/CustomDayRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Data.Repositories.Concrete
+{
+    public class CustomDayRepository
+    {
+    }
+}
