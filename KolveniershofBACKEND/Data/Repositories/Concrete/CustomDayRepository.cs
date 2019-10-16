109a583ac4656806575371c6270225bb87020ced
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-16 19:34:45 +0200

    Implement concrete day and customday repositories

--- KolveniershofBACKEND/Data/Repositories/Concrete/CustomDayRepository.cs
+++ KolveniershofBACKEND/Data/Repositories/Concrete/CustomDayRepository.cs
@@ -1,11 +1,70 @@
-﻿using System;
+﻿using KolveniershofBACKEND.Data.Repositories.Interfaces;
+using KolveniershofBACKEND.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KolveniershofBACKEND.Data.Repositories.Concrete
 {
-    public class CustomDayRepository
+    public class CustomDayRepository : ICustomDayRepository
     {
+        private DBContext _dbContext;
+        private DbSet<CustomDay> _customDays;
+
+        public CustomDayRepository(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+            _customDays = dbContext.CustomDays;
+        }
+
+        public IEnumerable<CustomDay> GetAll()
+        {
+            return _customDays.Include(cd => cd.Notes)
+                              .Include(cd => cd.DayActivities)
+                              .Include(cd => cd.Helpers)
+                              .ToList();
+        }
+
+        public IEnumerable<CustomDay> GetAllInRange(DateTime start, DateTime end)
+        {
+            return _customDays.Where(cd => cd.Date >= start && cd.Date <= end)
+                              .Include(cd => cd.Notes)
+                              .Include(cd => cd.DayActivities)
+                              .Include(cd => cd.Helpers)
+                              .ToList();
+        }
+
+        public CustomDay GetByDate(DateTime date)
+        {
+            return _customDays.Include(cd => cd.Notes)
+                              .Include(cd => cd.DayActivities)
+                              .Include(cd => cd.Helpers)
+                              .SingleOrDefault(cd => cd.Date == date);
+        }
+
+        public CustomDay GetById(int id)
+        {
+            return _customDays.Include(cd => cd.Notes)
+                              .Include(cd => cd.DayActivities)
+                              .Include(cd => cd.Helpers)
+                              .SingleOrDefault(cd => cd.DayId == id);
+        }
+
+        public void Add(CustomDay customDay)
+        {
+            _customDays.Add(customDay);
+        }
+
+        public void Remove(CustomDay customDay)
+        {
+            _customDays.Remove(customDay);
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
     }
 }
