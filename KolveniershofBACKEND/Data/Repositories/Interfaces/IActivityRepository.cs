7768c78373b894f5dbb85f96e529568cc090f5d8
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-16 18:18:46 +0200

    Add repository interfaces

---
+++ KolveniershofBACKEND/Data/Repositories/Interfaces/IActivityRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Data.Repositories.Interfaces
+{
+    public interface IActivityRepository
+    {
+    }
+}
