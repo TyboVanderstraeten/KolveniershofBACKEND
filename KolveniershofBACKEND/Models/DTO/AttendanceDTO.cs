c797f755761e26902ce5d0101284288f7678a20b
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-22 19:25:19 +0200

    Implement attendanceDTO

--- KolveniershofBACKEND/Models/DTO/AttendanceDTO.cs
+++ KolveniershofBACKEND/Models/DTO/AttendanceDTO.cs
@@ -8,9 +8,21 @@ namespace KolveniershofBACKEND.Models.DTO
 {
     public class AttendanceDTO
     {
+        public int DayId { get; set; }
+        public int ActivityId { get; set; }
+        public int UserId { get; set; }
+        public DayActivity DayActivity { get; set; }
+        public User User { get; set; }
+        public string Comment { get; set; }
+
         public AttendanceDTO(Attendance attendance)
         {
-
+            DayId = attendance.DayId;
+            ActivityId = attendance.ActivityId;
+            UserId = attendance.UserId;
+            DayActivity = attendance.DayActivity;
+            User = attendance.User;
+            Comment = attendance.Comment;
         }
     }
 }
