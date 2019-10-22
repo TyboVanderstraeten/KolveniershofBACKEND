e56651925026087c80f7b7e6049f5a1a46371574
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-22 19:37:21 +0200

    Add activityDTO

---
+++ KolveniershofBACKEND/Models/DTO/ActivityDTO.cs
@@ -0,0 +1,26 @@
+﻿using KolveniershofBACKEND.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Models.DTO
+{
+    public class ActivityDTO
+    {
+        public int ActivityId { get; set; }
+        public ActivityType ActivityType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Pictogram { get; set; }
+
+        public ActivityDTO(Activity activity)
+        {
+            ActivityId = activity.ActivityId;
+            ActivityType = activity.ActivityType;
+            Name = activity.Name;
+            Description = activity.Description;
+            Pictogram = activity.Pictogram;
+        }
+    }
+}
