259f091ec33cc97553de01d467a571f450060695
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-23 10:47:45 +0200

    Add helperDTO

---
+++ KolveniershofBACKEND/Models/DTO/HelperDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Models.DTO
+{
+    public class HelperDTO
+    {
+        public int DayId { get; set; }
+        public int UserId { get; set; }
+    }
+}
