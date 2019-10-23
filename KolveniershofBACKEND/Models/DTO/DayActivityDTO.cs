ec0becad162045c7f4ca21aa73330f15f2d52116
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-23 10:56:22 +0200

    Add dayactivityDTO

---
+++ KolveniershofBACKEND/Models/DTO/DayActivityDTO.cs
@@ -0,0 +1,16 @@
+﻿using KolveniershofBACKEND.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Models.DTO
+{
+    public class DayActivityDTO
+    {
+        public int DayId { get; set; }
+        public int ActivityId { get; set; }
+        public TimeOfDay TimeOfDay { get; set; }
+        public ICollection<AttendanceDTO> Attendances { get; set; }
+    }
+}
