90f9499fdae3b40aff82c3f498bc8c6df7f8cb6f
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-23 10:54:52 +0200

    Add day and customdayDTO

---
+++ KolveniershofBACKEND/Models/DTO/CustomDayDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Models.DTO
+{
+    public class CustomDayDTO:DayDTO
+    {
+        public DateTime Date { get; set; }
+        public string Menu { get; set; }
+        public ICollection<NoteDTO> Notes { get; set; }
+    }
+}
---
+++ KolveniershofBACKEND/Models/DTO/DayDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KolveniershofBACKEND.Models.DTO
+{
+    public class DayDTO
+    {
+        public int WeekNr { get; set; }
+        public int DayNr { get; set; }
+        public ICollection<DayActivityDTO> DayActivities { get; set; }
+        public ICollection<HelperDTO> Helpers { get; set; }
+    }
+}
