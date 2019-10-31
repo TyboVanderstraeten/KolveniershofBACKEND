cc81103f57ead57e255aef952f7f7dbc9ee37dd2
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-10-31 11:22:19 +0100

    Add getallforactivity method

--- KolveniershofBACKEND/Controllers/AttendancesController.cs
+++ KolveniershofBACKEND/Controllers/AttendancesController.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using KolveniershofBACKEND.Data.Repositories.Interfaces;
+using KolveniershofBACKEND.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KolveniershofBACKEND.Controllers
 {
-    public class AttendancesController
+    [ApiController]
+    [Route("KolveniershofAPI/[controller]")]
+    public class AttendancesController : ControllerBase
     {
+        private readonly ICustomDayRepository _customDayRepository;
+
+        public AttendancesController(ICustomDayRepository customDayRepository)
+        {
+            _customDayRepository = customDayRepository;
+        }
+
+        [HttpGet]
+        [Route("activity/{date}/{id}/{timeOfDay}")]
+        public ActionResult<IEnumerable<Attendance>> GetAllForActivity(DateTime date, int id, TimeOfDay timeOfDay)
+        {
+            DayActivity dayActivity = _customDayRepository
+                                            .GetByDate(date)
+                                            .DayActivities
+                                            .SingleOrDefault(da => da.ActivityId == id && da.TimeOfDay.Equals(timeOfDay));
+            return dayActivity.Attendances.ToList();
+        }
+
+        
+
     }
 }
