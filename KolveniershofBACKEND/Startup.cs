1a806621fdb088bc87c466b97bd0aec3e8ba5b66
TyboVanderstraeten <vanderstraeten.tybo@hotmail.com>
2019-11-06 18:55:22 +0100

    Add dependency injection for new repos

--- KolveniershofBACKEND/Startup.cs
+++ KolveniershofBACKEND/Startup.cs
@@ -48,7 +48,11 @@ namespace KolveniershofBACKEND
                 .AddScoped<IActivityRepository, ActivityRepository>()
                 .AddScoped<ICustomDayRepository, CustomDayRepository>()
                 .AddScoped<IDayRepository, DayRepository>()
-                .AddScoped<IUserRepository, UserRepository>();
+                .AddScoped<IUserRepository, UserRepository>()
+                .AddScoped<INoteRepository, NoteRepository>()
+                .AddScoped<IDayActivityRepository, DayActivityRepository>()
+                .AddScoped<IHelperRepository, HelperRepository>()
+                .AddScoped<IWeekendDayRepository, WeekendDayRepository>();
             #endregion
 
             #region NSwag
